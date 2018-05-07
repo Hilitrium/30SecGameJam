8bcae8733d3fe8d762320fb87b36d77894ee351d
Caleb Seay <s179067@students.aie.edu.au>
2018-05-07 13:55:27 -0700

    added in timer and finish line script

---
+++ Assets/CalebImport/Scripts/TimerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour {
+
+    public float Timer;
+    public Text CountDown;
+
+	// Use this for initialization
+	void Start ()
+    {
+        CountDown.text = "Time Left: " + Timer.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Timer -= Time.deltaTime;
+        CountDown.text = "Time Left: " + Timer.ToString();
+        if (Timer < 0)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("LossScene");
+    }
+
+}
